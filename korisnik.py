d4951be8863898bdd87f498f96cb7b2e885b6dd1
teofeel <teodor.perun@gmail.com>
2023-10-31 10:16:57 +0100

    Update korisnik.py

--- korisnik.py
+++ korisnik.py
@@ -4,10 +4,10 @@
 
 class Korisnik:
     def __init__(self, korisnicko_ime, lozinka, ime, prezime, uloga):
-        self.korisnicko_ime =           korisnicko_ime
-        self.lozinka        =           lozinka
-        self.ime            =           ime
-        self.prezime        =           prezime
-        self.uloga          =           uloga
+        self.korisnicko_ime = korisnicko_ime
+        self.lozinka        = lozinka
+        self.ime            = ime
+        self.prezime        = prezime
+        self.uloga          = uloga
 
     

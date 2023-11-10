c8c9e7727d76a8342facd6bcca21fba7c4ed888c
teofeel <teodor.perun@gmail.com>
2023-11-10 15:53:08 +0100

    dodavanje menadzera

--- functions/menadzer.py
+++ functions/menadzer.py
@@ -7,24 +7,27 @@ def main(korisnicko_ime):
     while True:
         print('Ulogovani Menadzer')
         print('1. Dodaj novog prodavca')
-        print('2. Izmena licnih podataka')
-        print('3. Odjava')
-        print('4. Izlazak iz aplikacije')
+        print('2. Dodaj novog menadzera')
+        print('3. Izmena licnih podataka')
+        print('4. Odjava')
+        print('5. Izlazak iz aplikacije')
 
         unos = input('Izaberite: ')
         if not unos.isdigit(): continue
         unos = int(unos)
 
         if unos==1:
-            dodaj_prodavca()
+            dodaj_zaposlenog()
         elif unos==2:
-            ulogovani_korisnici.izmena_licnih_podataka(_id)
+            dodaj_zaposlenog('menadzer')
         elif unos==3:
-            return
+            ulogovani_korisnici.izmena_licnih_podataka(_id)
         elif unos==4:
+            return
+        elif unos==5:
             exit()
 
-def dodaj_prodavca():
+def dodaj_zaposlenog(zaposleni='prodavac'):
     while True:
         print('Da se vratite unesite ;')
 
@@ -50,13 +53,14 @@ def dodaj_prodavca():
         if lozinka==';':return
         while ';'in lozinka or (not any(i.isdigit() for i in lozinka)) or len(lozinka)<6:
             lozinka = input('Lozinka mora biti duza od 6 karaktera, ne sme da sadrzi ; i mora sadrzati barem jednu cifru. Unesite opet lozinku: ')
+            if lozinka==';':return
 
         novi_prodavac = {
             "korisnicko_ime":korisnicko_ime, 
             "lozinka":lozinka,
             "ime":ime,
             "prezime":prezime,
-            "uloga":"prodavac"
+            "uloga":zaposleni
         }
 
         korisnik.novi_korisnik(novi_prodavac)
